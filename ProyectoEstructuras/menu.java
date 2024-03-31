cb4aed6d9c8b36df7b71d08efe470d91e2c87548
NQM765 <162798758+NQM765@users.noreply.github.com>
2024-03-31 08:15:31 -0500

    Add files via upload

--- ProyectoEstructuras/menu.java
+++ ProyectoEstructuras/menu.java
@@ -2,7 +2,7 @@
 * Clase que implementa un menú interactivo para un programa de gestión de tarjetas.
 * Permite al usuario realizar diversas operaciones como crear tarjetas, ver tarjetas
 * almacenadas y salir del programa.
-*/
+ */
 package ProyectoEstructuras;
 
 import java.util.InputMismatchException;
@@ -15,16 +15,17 @@ public class menu {
     * Método principal que inicia la ejecución del programa.
     * Crea un objeto Scanner para leer la entrada del usuario y una lista para
     * almacenar las tarjetas creadas.
-    */
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Lista<Tarjeta> lista = new Lista<>(); // Crear una instancia de Lista para almacenar las tarjetas
+        Lista<Tarjeta> guardadas = new Lista<>();
         int opcion, insertContinue;
 
         /*
         * Bucle principal que muestra el menú y procesa las opciones del usuario
         * hasta que el usuario elija salir del programa.
-        */
+         */
         do {
             try {
                 System.out.println("Menu:");
@@ -33,18 +34,18 @@ public class menu {
                 System.out.println("3. Ver tarjetas guardadas");
                 System.out.println("4. Salir");
                 System.out.print("Ingrese el numero de la opcion deseada: ");
-
                 opcion = scanner.nextInt(); // Lee la opción del usuario
                 scanner.nextLine(); // Consumir la nueva línea después de leer el número
-
+                System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                 switch (opcion) {
                     case 1:
                         /*
                         * Submenú para crear tarjetas.
                         * Permite al usuario ingresar información para crear nuevas tarjetas
                         * y almacenarlas en la lista.
-                        */
+                         */
                         do {
+                            System.out.println("\u001B[1mCreacion del consejo\u001B[0m");
                             System.out.println("Ingrese el titulo del consejo");
                             String tarjetaTitle = scanner.nextLine();
 
@@ -60,6 +61,7 @@ public class menu {
                             System.out.println("Ingrese '1' para seguir ingresando o ingrese '0' para dejar de ingresar");
                             insertContinue = scanner.nextInt(); // Lee la opción del usuario
                             scanner.nextLine(); // Consumir la nueva línea después de leer el número
+                            System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
                         } while (insertContinue != 0);
                         break;
@@ -68,17 +70,17 @@ public class menu {
                         /*
                         * Opción para ver tarjetas.
                         * Muestra las tarjetas almacenadas en la lista al usuario.
-                        */
+                         */
                         Iterator<Tarjeta> iterador = lista.iterator();
 
                         int tarjetaContinue = 1;
 
                         while (iterador.hasNext() && tarjetaContinue == 1) {
                             Tarjeta actualTarjeta = iterador.next();
-
+                            System.out.println("\u001B[1mTarjetas en la lista\u001B[0m");
                             System.out.println(actualTarjeta.getTitle());
                             System.out.println(actualTarjeta.getData());
-                            System.out.println("Etiquetas: " + actualTarjeta.getTag());
+                            System.out.println("Etiqueta: " + actualTarjeta.getTag());
 
                             if (iterador.hasNext()) {
                                 System.out.println("Ingrese '1' para ver la siguiente tarjeta o ingrese '0' para dejar de ver tarjetas");
@@ -86,6 +88,9 @@ public class menu {
                                 scanner.nextLine(); // Consumir la nueva línea después de leer el número
                             } else {
                                 System.out.println("No hay mas cartas para mostrar");
+                                System.out.println("Presione Enter para continuar...");
+                                scanner.nextLine(); // Espera a que el usuario presione Enter
+                                System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                             }
                         }
                         break;
@@ -94,9 +99,11 @@ public class menu {
                         /*
                         * Opción para ver las tarjetas guardadas.
                         * Muestra las tarjetas almacenadas en la lista al usuario.
-                        */
-                        System.out.println("Tarjetas guardadas:");
-                        lista.display(); // Muestra las tarjetas guardadas
+                         */
+                        System.out.println("\u001B[1mTarjetas guardadas:\u001B[0m");
+                        System.out.println("Presione Enter para continuar...");
+                        scanner.nextLine(); // Espera a que el usuario presione Enter
+                        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
                         break;
 
                     case 4:
@@ -109,7 +116,7 @@ public class menu {
                 /*
                 * Manejo de excepción para asegurar que el programa no se bloquee
                 * si el usuario ingresa un valor no válido.
-                */
+                 */
                 System.out.println("Error: Debe ingresar un numero entero.");
                 scanner.nextLine(); // Limpiar el buffer del scanner
                 opcion = 0; // Establecer opción en 0 para repetir el ciclo
